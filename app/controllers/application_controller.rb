d5095c463877b047b883dceea5818f2acf5f2029
ahmedhamid13 <eng.a.abdelhamid@outlook.com>
2023-06-28 18:44:54 +0300

    update link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,8 +8,8 @@ class ApplicationController < ActionController::API
     render json: {
       postman_collection_url: "https://documenter.getpostman.com/view/12318086/UVRGDP8s",
       postman_json_url: "https://www.postman.com/collections/6ed2730407949c6a8158",
-      site_performance: "https://chat-system-insta.herokuapp.com/admin/site_performance",
-      sidekiq_web: "https://chat-system-insta.herokuapp.com/admin/sidekiq",
+      site_performance: "https://chat-system-vdyt.onrender.com/admin/site_performance",
+      sidekiq_web: "https://chat-system-vdyt.onrender.com/admin/sidekiq",
     }
   end
 
